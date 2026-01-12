2914691f3c1935d27a6ef15575ed7b5eaad1939e
Eric Pedley <ericpedley@gmail.com>
2026-01-12 09:31:48 -0800

    set up new mode for nn control

--- src/main/msp/msp_box.c
+++ src/main/msp/msp_box.c
@@ -102,6 +102,7 @@ static const box_t boxes[CHECKBOX_ITEM_COUNT] = {
     { .boxId = BOXBEEPERMUTE, .boxName = "BEEPER MUTE", .permanentId = 52},
     { .boxId = BOXREADY, .boxName = "READY", .permanentId = 53},
     { .boxId = BOXLAPTIMERRESET, .boxName = "LAP TIMER RESET", .permanentId = 54},
+    { .boxId = BOXNNCONTROL, .boxName = "NN CONTROL", .permanentId = 55},
 };
 
 // mask of enabled IDs, calculated on startup based on enabled features. boxId_e is used as bit index
@@ -342,6 +343,8 @@ void initActiveBoxIds(void)
 #if defined(USE_GPS_LAP_TIMER)
     BME(BOXLAPTIMERRESET);
 #endif
+
+    BME(BOXNNCONTROL);
 
 #undef BME
     // check that all enabled IDs are in boxes array (check may be skipped when using findBoxById() functions)
